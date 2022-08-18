a3f2d5909c25d34f85b6f399870b04e230d475ef
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-08-18 18:47:14 +0530

    responsive header code add

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+
+
+function togglemenu(){
+    const menu = document.querySelector('.menu-btn')
+    const nav = document.querySelector('.navbar')
+    menu.classList.toggle('active')
+    nav.classList.toggle('active')
+}
